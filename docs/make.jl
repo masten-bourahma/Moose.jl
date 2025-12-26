d6e42ac08fa7443958489d536e27c08dbde18b40
masten-bourahma <bourahmamasten@gmail.com>
2025-12-26 09:39:07 +0100

    fixed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -32,7 +32,7 @@ makedocs(  sitename="Moose.jl",
            modules = [Moose],
            pages   = [ "Home"             => "index.md", # Main page
                        "Context"          => "context.md",
-                       "Low level usage"  => "_generated/Example.md",
+                       "Basic usage"  => "_generated/Example.md",
                        "Usage examples"   => "examples.md",
                        "API reference"    => "api_reference.md"],
            doctest = :true,
